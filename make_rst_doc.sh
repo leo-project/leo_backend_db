7eba55a2d5b2ac0308da4825e7196043439cac6f
Yosuke Hara <yousuke.hara@gmail.com>
2014-10-01 15:23:31 +0900

    Import making rst script

---
+++ make_rst_doc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf doc/rst && mkdir doc/rst
+pandoc --read=html --write=rst doc/leo_backend_db_api.html -o doc/rst/leo_backend_db_api.rst
+pandoc --read=html --write=rst doc/leo_backend_db_bitcask.html -o doc/rst/leo_backend_db_bitcask.rst
+pandoc --read=html --write=rst doc/leo_backend_db_eleveldb.html -o doc/rst/leo_backend_db_eleveldb.rst
+pandoc --read=html --write=rst doc/leo_backend_db_ets.html -o doc/rst/leo_backend_db_ets.rst
+pandoc --read=html --write=rst doc/leo_backend_db_server.html -o doc/rst/leo_backend_db_server.rst
