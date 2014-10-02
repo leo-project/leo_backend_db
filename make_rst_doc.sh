65096eb32b8cbcd17ae14ccfee51872aa333a6c2
Yosuke Hara <yousuke.hara@gmail.com>
2014-10-02 09:42:57 +0900

    Modify script of making doc

--- make_rst_doc.sh
+++ make_rst_doc.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm -rf doc/rst && mkdir doc/rst
+make doc
 pandoc --read=html --write=rst doc/leo_backend_db_api.html -o doc/rst/leo_backend_db_api.rst
 pandoc --read=html --write=rst doc/leo_backend_db_bitcask.html -o doc/rst/leo_backend_db_bitcask.rst
 pandoc --read=html --write=rst doc/leo_backend_db_eleveldb.html -o doc/rst/leo_backend_db_eleveldb.rst
